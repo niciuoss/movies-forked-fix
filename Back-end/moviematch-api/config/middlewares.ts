2b18b34e12d6679786b467e59aeba4279bf7120e
niciuoss <viniciussales.dev@gmail.com>
2025-12-04 18:45:37 -0300

    feat/configuration CORS for frontend integration

--- Back-end/moviematch-api/config/middlewares.ts
+++ Back-end/moviematch-api/config/middlewares.ts
@@ -1,8 +1,15 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173', 'http://localhost:3000'],
+      headers: '*',
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
